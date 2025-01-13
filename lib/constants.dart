d6a753d772c687de0874ab9f94a916c147cd8926
Eugene Ow <86452186+EugeneOw@users.noreply.github.com>
2025-01-13 20:36:36 +0800

    Commited

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,7 +1,4 @@
-List<String> formulaList = [
-  'Magnetic Flux Integral (ΦB = ∫∫B·dS)',
-  'Induced EMF in a loop (E = - dΦB/dt)',
-];
+List<String> formulaList = ['Magnetic Flux Integral (ΦB = ∫∫B·dS)','Induced EMF in a loop (E = - dΦB/dt)',];
 
 final inputs = ['B', 'BD', 'S', 'SD', 'A', 'P', 'dFlux', 'dt'];
 
