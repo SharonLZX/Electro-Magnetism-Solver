908d477f4047ca9f541cdfd790a0f07c86144ed8
SharonLZX <luzixing@live.com.sg>
2025-01-24 23:37:02 +0800

    modified:   lib/app.dart 	modified:   lib/features/presentations/page/calculate_page.dart 	new file:   lib/features/presentations/page/graph_page.dart 	modified:   pubspec.yaml

---
+++ lib/features/presentations/page/graph_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class GraphPage extends StatefulWidget {
+  const GraphPage({super.key});
+
+  @override
+  State<GraphPage> createState() => _GraphPageState();
+}
+
+class _GraphPageState extends State<GraphPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text("Hello World"),
+          ],
+        ),
+      ),
+    );
+  }
+}
