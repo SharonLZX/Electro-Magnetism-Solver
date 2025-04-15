b8328961710473a2a8a53ef48c8923ca6a4bb3b6
SharonLZX <luzixing@live.com.sg>
2025-04-15 22:41:42 +0800

    commit

--- lib/utils/handlers/multiplication/implicit_exponent.dart
+++ lib/utils/handlers/multiplication/implicit_exponent.dart
@@ -10,8 +10,8 @@ class ImplicitExponent {
     Now that we're expanding the multiplier and argument,
     each bracket can contain one of these 4 kinds of equation,
 
-    1) Constant (i.e. 5, etc.)
-    2) Coeff w variable (i.e. 5x, etc.)
+    1) Constant (i.e. 5x^0=5, etc.)
+    2) Coeff w variable (i.e. 5x^1=5x, etc.)
     3) Variable w exponent (i.e. x^2, etc.)
     4) Coeff w variable & exponent (i.e. 5x^2, etc.) 
     5) trigo variations
@@ -24,7 +24,7 @@ class ImplicitExponent {
     All equations will include a variable, either to the power
     of zero or 1, as well as a coefficient of 1 is required.
     */
-    
+
     // Creating the instance of the class.
     ContainTrigo containTrigo = ContainTrigo();
     Expansion expansion = Expansion();
@@ -36,12 +36,13 @@ class ImplicitExponent {
     List<String?> lstMultiplierArgument = [];
 
     // Check if multiplier is null, if so, return null.
-    if (multiplier == null){
+    if (multiplier == null) {
       return null;
     }
 
     // Check if multiplier or argument contain trigo, if so, return null.
-    if (containTrigo.containTrigo(multiplier) || containTrigo.containTrigo(argument)){
+    if (containTrigo.containTrigo(multiplier) ||
+        containTrigo.containTrigo(argument)) {
       return null;
     }
 
